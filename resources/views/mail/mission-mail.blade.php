0c428ab2478c74738042e0686649181aae19086f
Ilo <ilocacsu@gmail.com>
2024-05-27 14:05:02 +0300

    modification template mail mission

--- resources/views/mail/mission-mail.blade.php
+++ resources/views/mail/mission-mail.blade.php
@@ -1,19 +1,19 @@
-@component('mail::message')
+<x-mail::message>
     # Convocation pour une mission
 
     Cher/Chère {{ $fullName }},
 
     Nous avons le plaisir de vous convier à une mission qui se tiendra du {{ $date_debut }} au {{ $date_fin }}.
 
-    *Lieu de la mission : {{ $lieu }}*
+    Lieu de la mission : {{ $lieu }}
 
-    *Observations : {{ $observations }}*
+    Observations : {{ $observations }}
 
     Nous comptons sur votre présence active et votre contribution pour le succès de cette mission.
 
-    **N'oublier pas notre cotisation sociale de 10.000 Ariary, qui est à payer auprès de Monsieur José une fois la mission
-    terminée.**
+    N'oublier pas notre cotisation sociale de 10.000 Ariary, qui est à payer auprès de Monsieur José une fois la mission
+    terminée.
 
     Cordialement,
     {{ config('app.name') }}
-@endcomponent
+</x-mail::message>
