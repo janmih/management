7913e0d67dcae620bba8528629a3babcb93d7098
Ilo <ilocacsu@gmail.com>
2024-05-27 13:28:42 +0300

    modification template mail mission

--- resources/views/mail/mission-mail.blade.php
+++ resources/views/mail/mission-mail.blade.php
@@ -5,14 +5,15 @@
 
     Nous avons le plaisir de vous convier à une mission qui se tiendra du {{ $date_debut }} au {{ $date_fin }}.
 
-    **Lieu de la mission :** {{ $lieu }}
+    <strong>Lieu de la mission :</strong> {{ $lieu }}
 
-    **Observations :**
+    <strong>Observations :</strong>
     {{ $observations }}
 
     Nous comptons sur votre présence active et votre contribution pour le succès de cette mission.
 
-    N'oublier pas notre cotisation sociale de 10.000 Ariary.
+    <em>N'oublier pas notre cotisation sociale de 10.000 Ariary, qui est à payer auprès de Monsieur José une fois la mission
+        terminée.</em>
 
     Cordialement,
     {{ config('app.name') }}
